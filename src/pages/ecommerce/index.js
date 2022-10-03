0a44beca6fd2f827780ba04eb96b98bbb8a70783
larissambn <larissameo1@gmail.com>
2022-10-03 14:28:03 -0300

    index

--- src/pages/ecommerce/index.js
+++ src/pages/ecommerce/index.js
@@ -1,1 +1,60 @@
-testando;
+import { useState } from "react";
+import { api } from "../../api/api";
+import { Cart } from "./App.js";
+import { SearchBar } from "./searchBar.js";
+
+export function Teas() {
+  const [Teas, setTeas] = useState([]);
+
+  useEffect(() => {
+    async function fetchTeas() {
+      try {
+        const response = await axios.get("https://ironrest.herokuapp.com/...");
+
+        setTeas([...response.data]);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchTeas();
+  }, []);
+
+  function handleSubmit(e) 
+  //{  setForm({ ...form, [e.target.name]: e.target.value });}
+
+  return (
+    <>
+      <div className="Ecommerce">
+        <h1 id="Shop"> Shop </h1>
+        <SearchBar/>
+        <div className="flex">
+          <div className="flex-item">
+            {Teas.map((currentTea) => {
+              return (
+                <div className="TeaEcommerce">
+                  <>
+                    <Link to={`/${currentTea._id}`}>
+                      <img
+                        src={currentTea.image}
+                        alt={currentTea.name}
+                        id="currentTea.image"
+                      />
+                       <h1>{currentTea.name}</h1>
+                    </Link>
+                  </>
+                  <button id="btn" onClick={handleSubmit}>
+                      Add to Cart
+                    </button>                  
+                </div>
+              );
+            })}
+          </div>
+          <div id="Cart">
+            {" "}
+            <Cart />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
