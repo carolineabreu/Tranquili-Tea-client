c9e4e571a94cea39b95c51b0f76a57bf2cb406f9
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-10-04 12:02:36 -0300

    change profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,8 +27,8 @@ module.exports = {
         4: '4',
       },
       colors: {
-        sky: colors.sky,
-        cyan: colors.cyan,
+        // sky: colors.sky,
+        // cyan: colors.cyan,
         'white': '#ffffff',
         'purple': '#3f3cbb',
         'midnight': '#121063',
@@ -38,7 +38,7 @@ module.exports = {
         'bubble-gum': '#ff77e9',
         'bermuda': '#78dcca',
         'red': '#fef2f2',
-        'carnation': {  DEFAULT: '#F75C4B',  '50': '#FFFBFB',  '100': '#FEE9E7',  '200': '#FCC6C0',  '300': '#FAA399',  '400': '#F97F72',  '500': '#F75C4B',  '600': '#F52B15',  '700': '#C91C09',  '800': '#931407',  '900': '#5D0D04'},
+        'carnation': { DEFAULT: '#F75C4B', '50': '#FFFBFB', '100': '#FEE9E7', '200': '#FCC6C0', '300': '#FAA399', '400': '#F97F72', '500': '#F75C4B', '600': '#F52B15', '700': '#C91C09', '800': '#931407', '900': '#5D0D04' },
       },
     },
     variants: {
@@ -47,10 +47,10 @@ module.exports = {
     extend: {},
   },
   presets: [require('full-palette')],
-  
+
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('tw-elements/dist/plugin'),
     require('tailwindcss/colors')
   ],
-}
+};
