02ed3ba439f32320d8b93ac35051201bf09012ae
larissambn <larissameo1@gmail.com>
2022-10-04 11:49:37 -0300

    commit front

--- src/pages/ecommerce/payment.js
+++ src/pages/ecommerce/payment.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function payment() {
+export function Payment() {
   const [form, setForm] = useState({
     address: "",
     zipCode: "",
@@ -17,47 +17,46 @@ export function payment() {
     e.preventDefault();
     window.onload = "/orderHistory";
   }
+  return (
+    <>
+      <form onSubmit={handleSumit}>
+        <label>Address:</label>
+        <input
+          type="text"
+          name="address"
+          value={form.address}
+          onChange={handleChange}
+        />
+        <label>Zip Code</label>
+        <input
+          type="text"
+          name="zipCode"
+          value={form.zipCode}
+          onChange={handleChange}
+        />
+        <label>Discount Coupon</label>
+        <input
+          type="text"
+          name="discountCoupon"
+          value={form.discountCoupon}
+          onChange={handleChange}
+        />
+        <label>Card Number</label>
+        <input
+          type="number"
+          name="cardNumber"
+          value={form.cardNumber}
+          onChange={handleChange}
+        />
+        <label>Card Details</label>
+        <input
+          type="number"
+          name="cardCvc"
+          value={form.cardCvc}
+          onChange={handleChange}
+        />
+        <button type="submit">Buy</button>
+      </form>
+    </>
+  );
 }
-
-return (
-  <>
-    <form onSubmit={handleSumit}>
-      <label>Address:</label>
-      <input
-        type="text"
-        name="address"
-        value={form.address}
-        onChange={handleChange}
-      />
-      <label>Zip Code</label>
-      <input
-        type="text"
-        name="zipCode"
-        value={form.zipCode}
-        onChange={handleChange}
-      />
-      <label>Discount Coupon</label>
-      <input
-        type="text"
-        name="discountCoupon"
-        value={form.discountCoupon}
-        onChange={handleChange}
-      />
-      <label>Card Number</label>
-      <input
-        type="number"
-        name="cardNumber"
-        value={form.cardNumber}
-        onChange={handleChange}
-      />
-      <label>Card Details</label>
-      <input
-        type="number"
-        name="cardCvc"
-        value={form.cardCvc}
-        onChange={handleChange}
-      />
-      <button type="submit">Buy</button>
-    </form>
-  </>
-);
