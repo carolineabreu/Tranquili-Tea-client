c636d799c11567170184eb5150c375201b0145e8
larissambn <larissameo1@gmail.com>
2022-10-05 10:44:11 -0300

    ecommerce com tailwind

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,20 +1,15 @@
-import {Navbar} from "../../components/Navbar";
-import {Footer} from "../../components/Footer";
-import {Carousel} from "../../components/Carousel";
+import { Navbar } from "../../components/Navbar";
+import { Footer } from "../../components/Footer";
+import { Carousel } from "../../components/Carousel";
 import { HomeCards } from "../../components/HomeCards";
-import { Testimonials } from "../../components/Testimonials"
-
 
 export function Home() {
   return (
-<>
-<Navbar />
-<Carousel />
-<Footer />
-<HomeCards/>
-<Testimonials />
-
-</>
-
+    <>
+      <Navbar />
+      <Carousel />
+      <Footer />
+      <HomeCards />
+    </>
   );
 }
