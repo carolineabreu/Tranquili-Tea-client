b0163f1c3dd040fd64741d5d0536d8590c43bddd
larissambn <larissameo1@gmail.com>
2022-10-06 16:21:13 -0300

    app

--- src/pages/ecommerce/index.js
+++ src/pages/ecommerce/index.js
@@ -35,7 +35,6 @@ export function Teas() {
             <button
               type="button"
               className=" absolute top-5 right-0 h-30 w-30 text-white bg-stone-700 hover:bg-stone-900 flex justify-end focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              className="absolute top-5 right-0 h-30 w-30 text-white bg-stone-700 hover:bg-stone-900 flex justify-end focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               <svg
                 aria-hidden="true"
@@ -70,7 +69,6 @@ export function Teas() {
                   </p>
                   <></>
                   <button
-
                     type="button"
                     className="rounded-lg px-4 py-2 bg-green-300 hover:bg-green-400 duration-300 mr-2.5"
                     //onClick={addTocart}
