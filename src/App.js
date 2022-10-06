ab13cfa50cff72363d466554e4b81292279f7e8c
larissambn <larissameo1@gmail.com>
2022-10-06 00:13:55 -0300

    tentativas

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { Card } from "./pages/ecommerce/card.js";
 import { Teas } from "./pages/ecommerce/index.js";
 import { Reviews } from "./pages/ecommerce/reviews.js";
 import { CartPage } from "./pages/ecommerce/CartPage.js";
-// import { TeaDisplay } from "./pages/ecommerce/TeaDisplay.js";
+import { TeaDisplay } from "./pages/ecommerce/TeaDisplay.js";
 import { Payment } from "./pages/ecommerce/payment.js";
 import { OrderHistory } from "./pages/ecommerce/orderHistory.js";
 import { OurTeam } from "./pages/OurTeam/index.js";
@@ -40,8 +40,7 @@ function App() {
             <Route path="/Teas" element={<Teas />} />
             <Route path="/TeaPage/:id/review" element={<Reviews />} />
             <Route path="/ecommerce" element={<Teas />} />
-            {/* <Route path="/TeaDisplay" element={<TeaDisplay />} />
-            <Route path="/CartPage" element={<CartPage />} /> */}
+            <Route path="/TeaDisplay/:id" element={<TeaDisplay />} />
             <Route path="/CartPage" element={<CartPage />} />
             <Route path="/ecommerce/payment" element={<Payment />} />
             <Route path="/allTeas" element={<AllTeas />} />
--- src/pages/ecommerce/pages/AllTeas.js
+++ src/pages/ecommerce/pages/AllTeas.js
@@ -2,51 +2,33 @@ import { Card } from "../card.js";
 export function AllTeas() {
   return (
     <div>
-      //{" "}
+      {" "}
       <Card>
-        // teaName: "Lavender", teaImg: [ //
+        teaName= "Lavender" teaImg = "[ //
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSFbyPjIqZ3uqolWoR6E3W8WyITyQjBP_d-g&usqp=CAU",
-        // ], TeaDescription: "Lavender is a flowering plant in the mint family,
-        // with a great aroma and will help you to relax.", category:
-        ["Stressed", // "Anxious"], origin: "Albania", brew: "5 minutes",
-        temperature: 100, // caffeine: false, flavour: "Floral notes with an
-        earthy undertone.", // ingredients: ["Organic Lavender"], teaPrice:
-        9.98, //{" "}
-      </Card>
-      //{" "}
+        ]", TeaDescription = "Lavender is a flowering plant in the mint family,
+        with a great aroma and will help you to relax." teaPrice = "9.98"{" "}
+      </Card>{" "}
       <Card>
-        // teaName: "Lemon Balm",teaImg: [ //
+        teaName ="Lemon Balm" teaImg = "[ //
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNl8B9FAzja1YPa7JVLaQ4-tVh7kGJO-gYVQ&usqp=CAU",
-        // ], teaDescription: "Lemon balm belongs to the mint family, it has a
-        calm // effect, used historically for relieve the nervous tension.",
-        category: // ["Stressed", "Anxious"], origin: "Southern Europe", brew:
-        "3-5 minutes", // temperature: "93", caffeine: false, flavour: "
-        Pleasantly mild, with // subtle citrus notes.", ingredients: ["Organic
-        Lemon Balm"], teaPrice: // 7.98, //{" "}
-      </Card>
-      //{" "}
+        ], " teaDescription = "Lemon balm belongs to the mint family, it has a
+        calm effect, used historically for relieve the nervous tension.",
+        teaPrice = "7.98"{" "}
+      </Card>{" "}
       <Card>
-        // teaName: "Schisandra", teaImg: [ //
+        teaName: "Schisandra", teaImg: [
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkcz_X0kMsuClJof0c9DykNMIVEwLcQjjIWw&usqp=CAU",
-        // ], teaDescription: "Schisandra berry hails from wild, lush, and //
+        ], teaDescription: "Schisandra berry hails from wild, lush, and
         mountainous regions throughout China, it has adaptogenic properties //
-        which helps the body resist anxiety and stress.", category: ["Anxious",
-        // "Stressed", "Moody"], origin: "China", brew: "5 minutes",
-        temperature: // "93", caffeine: false, flavour: "Fruity with a uniquely
-        quenching // balance of five flavours: sweet, sour, salty, bitter,
-        pungent. ", // ingredients: ["Organic Schisandra"], teaPrice: 9.98, //{" "}
-      </Card>
-      //{" "}
+        which helps the body resist anxiety and stress." teaPrice: "9.98"
+      </Card>{" "}
       <Card>
-        // teaName: "Peppermint", teaImg: [ //
+        teaName: "Peppermint", teaImg:" [ //
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGztqawIe9Czep0s0m6PvxJ1kvsPFGJzz5VQ&usqp=CAU",
-        // ], TeaDescription: "The active ingredient in peppermint is menthol,
-        // which is said to stimulate passion and creativity.", category: //
-        ["Anxious"], origin: "Pacific Northwest", brew: "1 minute", temperature:
-        // 95, caffeine: false, flavour: "Fresh and cool minty flavour.", //
-        ingredients: ["Organic Peppermint "], teaPrice: 8.98, //{" "}
-      </Card>
-      //{" "}
+        ]", TeaDescription: "The active ingredient in peppermint is menthol, //
+        which is said to stimulate passion and creativity.", teaPrice = "8.98"
+      </Card>{" "}
     </div>
   );
 }
