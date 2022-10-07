329587315cb627618e34b449878225bf8fa2869f
larissambn <larissameo1@gmail.com>
2022-10-07 01:26:41 -0300

    funcionando a productlist

--- src/pages/ecommerce/Carrinho/components/Card.js
+++ src/pages/ecommerce/Carrinho/components/Card.js
@@ -1,9 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { CarrinhoContext } from "../../Carrinho/carrinho.js";
+import { useState, useEffect } from "react";
+import { api } from "../../../../api/api.js";
+import { Link } from "react-router-dom";
 
 export function Card(props) {
   const navigate = useNavigate();
+  const [tea, setTea] = useState([]);
 
   const carrinho = useContext(CarrinhoContext);
 
@@ -13,25 +17,41 @@ export function Card(props) {
     carrinho.setTea([...carrinho.tea, { ...props }]);
   }
 
+  useEffect(() => {
+    async function fetchTea() {
+      try {
+        const response = await api.get(`/tea/all`);
+        setTea(response.data);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchTea();
+  }, []);
+
   return (
     <div>
-      <h1>{props.teaName}</h1>
-      <p>{props.teaPrice}</p>
-      <button onClick={handleCarrinho}>Adicionar ao carrinho</button>
-      <button
-        onClick={() => {
-          navigate(`/${props.teaId}`);
-        }}
-      >
-        Ver produto
-      </button>
-      <div>
-        <img
-          src={props.teaImage}
-          alt="Tea"
-          style={{ width: "150px", height: "150px" }}
-        />
-      </div>
+      {tea.map((currentTea) => {
+        console.log(currentTea);
+        return (
+          <div key={currentTea.id}>
+            <h1>{currentTea.name}</h1>
+            <p>{currentTea.price}</p>
+            <button onClick={handleCarrinho}>Adicionar ao carrinho</button>
+            <Link to={`/teaDetail/${currentTea._id}`}>
+              <button>Ver produto</button>
+            </Link>
+            <div>
+              <img
+                src={currentTea.image}
+                alt="Tea"
+                style={{ width: "150px", height: "150px" }}
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/pages/ecommerce/Carrinho/pages/productList.js
+++ src/pages/ecommerce/Carrinho/pages/productList.js
@@ -1,73 +1,11 @@
 import { Card } from "../../Carrinho/components/Card.js";
-import { useEffect, useState } from "react";
-import { api } from "../../../../api/api.js";
 
 export function ProductList() {
-  const [tea, setTea] = useState([]);
-
-  useEffect(() => {
-    async function fetchTea() {
-      try {
-        const response = await api.get(`/tea/all`);
-        setTea(response.data);
-        console.log(response);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchTea();
-  }, []);
-
   return (
     <>
       <div>
-        <Card>
-          {tea.map((currentTea) => {
-            console.log(currentTea);
-            return (
-              <div key={currentTea.id}>
-                <h1>{currentTea.name}</h1>
-                <p>{currentTea.price}</p>
-                <button>Adicionar ao carrinho</button>
-                <button>Ver produto</button>
-                <div>
-                  <img
-                    src={currentTea.image}
-                    alt="Tea"
-                    style={{ width: "150px", height: "150px" }}
-                  />
-                </div>
-              </div>
-            );
-          })}
-        </Card>
+        <Card />
       </div>
-
-      {/* <Card
-        teaId="1"
-        teaTitle="Erva Cidreira"
-        teaDescription="só é gostoso mesmo"
-        teaImg="https://t2.uc.ltmcdn.com/pt/posts/8/3/3/como_plantar_capim_santo_20338_600_square.jpg"
-      />
-      <Card
-        teaId="2"
-        teaTitle="Erva doce"
-        teaDescription="só é gostoso mesmo"
-        teaImg="https://www.eusemfronteiras.com.br/wp-content/uploads/2017/07/lemon-5240181_1920-810x540.jpg"
-      />
-      <Card
-        teaId="3"
-        teaTitle="Capim santo"
-        teaDescription="nem é bom"
-        teaImg="https://t2.uc.ltmcdn.com/pt/posts/8/3/3/como_plantar_capim_santo_20338_600_square.jpg"
-      />
-      <Card
-        teaId="4"
-        teaTitle="Boldo"
-        teaDescription="pro estomago e ressaca"
-        teaImg="https://www.eusemfronteiras.com.br/wp-content/uploads/2017/07/lemon-5240181_1920-810x540.jpg"
-      />
-    </> */}
     </>
   );
 }
