c5e0ab47a01589bb292a95e8a896dfa9dc6a2b89
Zimarlen Silva <zimarlensilva@gmail.com>
2022-10-09 13:22:26 -0700

    css added to edit profile, login, and profile

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -29,7 +29,7 @@ export function Login() {
     <div className="contain py-16">
       <div className="max-w-lg mx-auto shadow px-6 py-7 rounded overflow-hidden">
         <h2 className="text-2xl uppercase font-medium mb-1">Login</h2>
-        <p className="text-gray-600 mb-6 text-sm">welcome back customer</p>
+        <p className="text-gray-600 mb-6 text-sm">Welcome back customer</p>
         <form onSubmit={handleSubmit} autoComplete="on">
           <div className="space-y-2">
             <div>
@@ -42,7 +42,7 @@ export function Login() {
                 name="email"
                 id="email"
                 value={form.email}
-                className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-400"
+                className="block w-full border bg-100 border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary placeholder-gray-9000"
                 placeholder="youremail.@domain.com"
               />
             </div>
@@ -89,7 +89,6 @@ export function Login() {
             </button>
           </div>
         </form>
-
         <div className="mt-6 flex justify-center relative">
           <div className="text-gray-600 uppercase px-3 bg-white z-10 relative">
             Or login with
@@ -99,18 +98,17 @@ export function Login() {
         <div className="mt-4 flex gap-4">
           <Link
             to="/"
-            className="w-1/2 py-2 text-center text-white bg-blue-400 rounded uppercase font-roboto font-medium text-sm hover:bg-blue-800"
+            className="w-1/2 py-2 text-center text-white bg-primary rounded uppercase font-roboto font-medium text-sm hover:bg-blue-800"
           >
             facebook
           </Link>
           <Link
             to="/"
-            className="w-1/2 py-2 text-center text-white bg-green-400 rounded uppercase font-roboto font-medium text-sm hover:bg-green-800"
+            className="w-1/2 py-2 text-center text-white bg-green-600 rounded uppercase font-roboto font-medium text-sm hover:bg-green-800"
           >
             google
           </Link>
         </div>
-
         <p className="mt-4 text-center text-gray-600">
           Don't have account?{" "}
           <Link to="/signup" className="text-primary">
