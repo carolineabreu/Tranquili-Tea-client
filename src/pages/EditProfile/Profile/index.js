ea65e5f8820b29eabe6639c0635c1bd3ee9d3b4a
Caroline Santos de Abreu <caroline@pop-os.localdomain>
2022-10-12 12:25:12 -0300

    fix profile, edit profile and signup

--- src/pages/EditProfile/Profile/index.js
+++ src/pages/EditProfile/Profile/index.js
@@ -43,28 +43,38 @@ export function EditProfile() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <h1>Customize Profile</h1>
-        <label htmlFor="formAbout">Address:</label>
-        <br />
-        <input
-          id="formAddress"
-          placeholder="Address"
-          type="text"
-          name="address"
-          onChange={handleChange}
-          value={form.address}
-        />
-        <br />
-        <input
-          id="formPhoneNumber"
-          placeholder="Phone Number"
-          type="number"
-          name="phoneNumber"
-          onChange={handleChange}
-          value={form.phoneNumber}
-        />
-        <br />
-        <button type="submit">Submit</button>
+        <div className="contain py-16">
+          <div className="max-w-lg mx-auto shadow px-6 py-7 rounded overflow-hidden">
+            <h4 className="text-lg font-medium capitalize mb-4">
+              Edit Profile
+            </h4>
+            <div>
+              <label htmlFor="phone">Phone number:</label>
+              <input
+                value={form.phoneNumber}
+                onChange={handleChange}
+                type="tel"
+                name="phone"
+                id="phone"
+                className="input-box input-box input-box block w-full border mb-4 text-sm border-gray-300 px-4 py-3 text-gray-600 rounded focus:ring-0 focus:border-primary focus:bg-blue-100 placeholder-gray-400"
+                placeholder="Fill in your phone number"
+              />
+            </div>
+            <div>
+              <label htmlFor="formAddress">Address:</label>
+              <input
+                value={form.address}
+                onChange={handleChange}
+                type="text"
+                name="address"
+                id="formAddress"
+                className="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded focus:ring-0 focus:border-primary focus:bg-blue-100 placeholder-gray-400"
+              />
+            </div>
+
+            <button type="submit" className="w-1/2 py-2 text-center text-white bg-green-400 rounded uppercase font-roboto font-medium text-sm hover:bg-green-600">Save</button>
+          </div>
+        </div>
       </form>
     </div>
   );
